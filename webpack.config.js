40df94165bca9c90e02dec76ca19c6fc4834990b
necm1 <necmi.m.hasan@outlook.de>
2021-11-18 06:13:53 +0100

    [CREATE] webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: {
+        extension: path.join(__dirname, 'src', 'extension.ts'),
+        'content-script': path.join(__dirname, 'src', 'content-script.ts'),
+        background: path.join(__dirname, 'src', 'background.ts'),
+    },
+    devtool: 'source-map',
+    output: {
+        publicPath: '.',
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js',
+    },
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                use: [
+                    {
+                      loader: 'ts-loader',
+                      options: {
+                        transpileOnly: true
+                      }
+                    }
+                ],
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                path.resolve(__dirname, 'manifest.json'),
+                path.resolve(__dirname, 'dist'),
+            ]
+        })
+    ]
+};
